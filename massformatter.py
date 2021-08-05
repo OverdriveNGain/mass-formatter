d4ffe44edcfef12e24f3f71059cf29040cf4f089
jeremy <jeremyamon@gmail.com>
2021-08-05 15:18:47 +0800

    Removed pixels; added short descriptions to each functional command

--- massformatter.py
+++ massformatter.py
@@ -15,7 +15,6 @@ from subscripts import retainmain
 from subscripts import timelapse
 from subscripts import imagestitch
 from subscripts import txt
-from subscripts import pixels
 from subscripts import filenameprint
 from subscripts import jpgcompress
 
@@ -34,31 +33,126 @@ print('''
 ''')
 
 commandMap = {
-	"replacecolor": lambda : replacecolor.start(),
-	"autocrop": lambda : autocrop.start(),
-	"resizer": lambda : resizer.start(),
-	"pdf2img": lambda : pdf2img.pdf2img(),
-	"img2pdf": lambda : pdf2img.img2pdf(),
-	"gifmaker": lambda : gifmaker.start(),
-	"boxbackup": lambda : boxbackup.backup(),
-	"boxrestore": lambda : boxbackup.restore(),
-	"separator": lambda : separator.start(),
-	"outliner": lambda : outliner.start(),
-	"divider": lambda : divider.start(),
-	"masscrop": lambda : masscrop.start(),
-	"massrenamer": lambda : massrenamer.start(),
-	"lighten": lambda : lighten.start(),
-	"flattenopacity": lambda : flattenopacity.start(),
-	"retainmain": lambda : retainmain.start(),
-	"timelapse": lambda : timelapse.start(),
-	"imagestitch": lambda : imagestitch.start(),
-	"boxclear": lambda : boxbackup.clear(),
-	"txt": lambda : txt.start(),
-	"pixels": lambda : pixels.start(),
-	"filenameprint": lambda : filenameprint.start(),
-	"jpgcompress": lambda : jpgcompress.start(),
-	"pdfstitch": lambda : pdf2img.pdfstitch(),
-	"help": lambda : helpAction(commandMap),
+	"replacecolor": 
+		{
+			"execute": lambda : replacecolor.start(),
+			"desciption": "Replaces all occurances of a certin color within an image by another color.",
+		},
+	"autocrop": 
+		{
+			"execute": lambda : autocrop.start(),
+			"desciption": "Automatically removes transparent margins around opaque blobs in a png image through cropping.",
+		},
+	"resizer": 
+		{
+			"execute": lambda : resizer.start(),
+			"desciption": "Resizes multiple images in bulk.",
+		},
+	"pdf2img": 
+		{
+			"execute": lambda : pdf2img.pdf2img(),
+			"desciption": "Creates image files from the pages of a PDF file.",
+		},
+	"img2pdf": 
+		{
+			"execute": lambda : pdf2img.img2pdf(),
+			"desciption": "Creates a pdf file from multiple image files.",
+		},
+	"gifmaker": 
+		{
+			"execute": lambda : gifmaker.start(),
+			"desciption": "Creates a gif image from multiple image files.",
+		},
+	"boxbackup": 
+		{
+			"execute": lambda : boxbackup.backup(),
+			"desciption": "",
+		},
+	"boxrestore": 
+		{
+			"execute": lambda : boxbackup.restore(),
+			"desciption": "",
+		},
+	"separator": 
+		{
+			"execute": lambda : separator.start(),
+			"desciption": "Seperates multiple opaque blobs that exists within a single image into multiple images.",
+		},
+	"outliner": 
+		{
+			"execute": lambda : outliner.start(),
+			"desciption": "",
+		},
+	"divider": 
+		{
+			"execute": lambda : divider.start(),
+			"desciption": "Slices an image into several smaller images.",
+		},
+	"masscrop": 
+		{
+			"execute": lambda : masscrop.start(),
+			"desciption": "Crops all images by certain crop values.",
+		},
+	"massrenamer": 
+		{
+			"execute": lambda : massrenamer.start(),
+			"desciption": "Renames files by a set filename and a set iteration value.",
+		},
+	"lighten": 
+		{
+			"execute": lambda : lighten.start(),
+			"desciption": "Simple lightening of image using linear interpolation of RGB values.",
+		},
+	"flattenopacity": 
+		{
+			"execute": lambda : flattenopacity.start(),
+			"desciption": "Manipulates the alpha values of each pixel of an image by assigning either a value of 0 or a value of 255, depending on a certain threshold.",
+		},
+	"retainmain": 
+		{
+			"execute": lambda : retainmain.start(),
+			"desciption": "Isolates the biggest non-opaque blob of pixels from other blobs within the same image.",
+		},
+	"timelapse": 
+		{
+			"execute": lambda : timelapse.start(),
+			"desciption": "Takes screenshots of your computer screen with a set periodic time.",
+		},
+	"imagestitch": 
+		{
+			"execute": lambda : imagestitch.start(),
+			"desciption": "Creates an .avi video file from a multiple images.",
+		},
+	"boxclear": 
+		{
+			"execute": lambda : boxbackup.clear(),
+			"desciption": "Clears box folder.",
+		},
+	"txt": 
+		{
+			"execute": lambda : txt.start(),
+			"desciption": "Provides functions for bulk manipulation of text.",
+		},
+	"filenameprint": 
+		{
+			"execute": lambda : filenameprint.start(),
+			"desciption": "Lists out file names with optional prefix and suffix strings.",
+		},
+	"jpgcompress": 
+		{
+			"execute": lambda : jpgcompress.start(),
+			"desciption": "Compresses image files into jpg files with variable quality.",
+		},
+	"pdfstitch": 
+		{
+			"execute": lambda : pdf2img.pdfstitch(),
+			"desciption": "",
+		},
+	"help": 
+		{
+			"execute": lambda : helpAction(commandMap),
+			"desciption": "Lists out the available commands.",
+		},
 }
 print("Version 1.0. Type 'help' for available commands")
 def helpAction(commandMap):
